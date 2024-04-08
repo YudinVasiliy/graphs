b24bca889e9a75e00e10c5ad2785b8995063d764
Vasiliy_Yudin <vasiliy_yudin_education@mail.ru>
2024-04-08 16:45:29 +0300

    Done ex 1514

--- ex1514.java
+++ ex1514.java
@@ -1,5 +1,6 @@
 package org.leetcode;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ex1514 {
@@ -14,15 +15,39 @@ public class ex1514 {
     } //модульный тест
     private static class Solution {
         public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-            class EdgeToVertex implements Comparable<EdgeToVertex>{
+            //a little optimized by myself someone else's algorithm, which is the best
+            double[] maxProb = new double[n];
+            maxProb[start_node] = 1.0;
+            boolean hasUpdate=true;
+            int u;
+            int v;
+            double pathProb;
+            for (int i = 0; i < n - 1 && hasUpdate; ++i) {
+                hasUpdate = false;
+                for (int j = 0; j < edges.length; ++j) {
+                    u = edges[j][0];
+                    v = edges[j][1];
+                    pathProb = succProb[j];
+                    if (maxProb[u] * pathProb > maxProb[v]) {
+                        maxProb[v] = maxProb[u] * pathProb;
+                        hasUpdate = true;
+                    }
+                    if (maxProb[v] * pathProb > maxProb[u]) {
+                        maxProb[u] = maxProb[v] * pathProb;
+                        hasUpdate = true;
+                    }
+                }
+            }
+            return maxProb[end_node];
+
+            //my slow algorithm, a lot of unnecessary actions
+            /*class EdgeToVertex implements Comparable<EdgeToVertex>{
                 double probability;
                 int endVertexInd;
-                int startVertexInd;
 
-                public EdgeToVertex(double probability, int eVertexInd,int sVertexInd) {
+                public EdgeToVertex(double probability, int eVertexInd) {
                     this.probability = probability;
                     this.endVertexInd = eVertexInd;
-                    this.startVertexInd = sVertexInd;
                 }
                 @Override
                 public int compareTo(EdgeToVertex o) {
@@ -32,73 +57,40 @@ public class ex1514 {
                 public boolean equals(Object o) {
                     if (!(o instanceof EdgeToVertex vertex)) return false;
                     //System.out.println("EQUALS: "+vertexInd+", "+vertex.vertexInd);
-                    return startVertexInd==vertex.startVertexInd &&
-                            endVertexInd == vertex.endVertexInd;
+                    return endVertexInd == vertex.endVertexInd;
                 }
                 @Override
                 public int hashCode() {
                     return Objects.hash(endVertexInd);
                 }
-
-                @Override
-                public String toString() {
-                    return "EdgeToVertex{" +
-                            "probability=" + probability +
-                            ", endVertexInd=" + endVertexInd +
-                            ", startVertexInd=" + startVertexInd +
-                            '}';
-                }
             }
-            HashMap<Integer,LinkedList<EdgeToVertex>> edgeToVertexMap=new HashMap<>();
+            HashMap<Integer, ArrayList<EdgeToVertex>> edgeToVertexMap=new HashMap<>();
             for(int i=0;i< edges.length;++i){
-                if(!edgeToVertexMap.containsKey(edges[i][0])){
-                    var edgeList=new LinkedList<EdgeToVertex>();
-                    edgeList.add(new EdgeToVertex(succProb[i], edges[i][1]));
-                    edgeToVertexMap.put(edges[i][0], edgeList);
-                }
-                else{
-                    edgeToVertexMap.get(edges[i][0]).add(new EdgeToVertex(succProb[i], edges[i][1]));
-                }
-                if(!edgeToVertexMap.containsKey(edges[i][1]))
-                    edgeToVertexMap.put(edges[i][1],new LinkedList<>());
-                edgeToVertexMap.get();
+                edgeToVertexMap.computeIfAbsent(edges[i][0], m -> new ArrayList<>()).add(new EdgeToVertex(succProb[i], edges[i][1]));
+                edgeToVertexMap.computeIfAbsent(edges[i][1], m -> new ArrayList<>()).add(new EdgeToVertex(succProb[i], edges[i][0]));
             }
             var probabilities=new double[n];
             probabilities[start_node]=1.0;
 
-
-            PriorityQueue<Vertex> queue=new PriorityQueue<>(adjacency.length);
-            for(int i=0;i<probabilities.length;++i){
-                queue.add(new Vertex(probabilities[i],i));
+            PriorityQueue<EdgeToVertex> queue=new PriorityQueue<>(n);
+            for(int i=0;i<n;++i){
+                queue.add(new EdgeToVertex(probabilities[i],i));
             }
-            //System.out.println("Queue first: -----------------------------");
-            //queue.forEach(System.out::println);
             while (!queue.isEmpty()){
                 var current=queue.remove();
-                //check siblings
-                //System.out.println("Queue before: -----------------------------");
-                //queue.forEach(System.out::println);
-                for(int i=0;i< adjacency.length;++i){
-                    if(i!= current.vertexInd &&
-                            probabilities[i]< current.probability*adjacency[current.vertexInd][i]){
-                        var sibling= new Vertex(probabilities[i],i);
-                        //System.out.println("======================");
-                        //System.out.println("4: "+queue.contains(new Vertex(1,4)));
-                        //System.out.println(sibling);
+                if(current.endVertexInd==end_node)return current.probability;
+                var siblings=edgeToVertexMap.getOrDefault(current.endVertexInd,new ArrayList<>());
+                for (EdgeToVertex sibling : siblings) {
+                    if (probabilities[sibling.endVertexInd] <
+                            current.probability * sibling.probability) {
                         queue.remove(sibling);
-                        sibling.probability=probabilities[i]=current.probability*adjacency[current.vertexInd][i];
-                        //System.out.println(sibling);
+                        sibling.probability = probabilities[sibling.endVertexInd] =
+                                current.probability * sibling.probability;
                         queue.add(sibling);
-                        //System.out.println("4: "+queue.contains(new Vertex(1,4)));
-                        //System.out.println(queue.contains(sibling)+"\n======================");
                     }
                 }
-                //System.out.println("--------------------------------------------------------------");
-                //System.out.println("Current="+current);
-                //System.out.println("Distances: "+Arrays.toString(probabilities));
-                //queue.forEach(System.out::println);
             }
-            return probabilities[end_node];
+            return probabilities[end_node];*/
         }
     }
 }
