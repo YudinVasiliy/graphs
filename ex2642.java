c173afd88a5d2bc773f8a1aa41dc0b70bf54d469
Vasiliy_Yudin <vasiliy_yudin_education@mail.ru>
2024-04-14 01:05:35 +0300

    2642 with ford-bellman without optimization(best) and option with dijcstra

--- ex2642.java
+++ ex2642.java
@@ -1,8 +1,6 @@
 package org.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class ex2642 {
 
@@ -11,8 +9,45 @@ public class ex2642 {
     } //модульный тест
 
     private static class Graph {
+        //without optimization
+        //ford-bellman
+        /*int numberOfNodes;
+        ArrayList<int[]> edges;
 
-        int numberOfNodes;
+        public Graph(int n, int[][] edges_) {
+            numberOfNodes=n;
+            edges= new ArrayList<>(Arrays.asList(edges_));
+        }
+
+        public void addEdge(int[] edge) {
+            edges.add(edge);
+        }
+
+        public int shortestPath(int node1, int node2) {
+            int toReturn;
+            int[] dist=new int[numberOfNodes];
+            Arrays.fill(dist,Integer.MAX_VALUE);
+            dist[node1] = 0;
+            boolean hasUpdate=true;
+            int[] edge;
+            for (int i = 0; i < numberOfNodes - 1 && hasUpdate; ++i) {
+                hasUpdate = false;
+                for (int j = 0; j < edges.size(); ++j) {
+                    edge = edges.get(j);
+                    if (dist[edge[0]] < dist[edge[1]]-edge[2]) {
+                        dist[edge[1]] = dist[edge[0]] +edge[2];
+                        hasUpdate = true;
+                    }
+                }
+            }
+            toReturn=dist[node2];
+            return toReturn!=Integer.MAX_VALUE?toReturn:-1;
+        }*/
+
+
+        //good, but have unnecessary optimization
+        //ford-bellman
+        /*int numberOfNodes;
         ArrayList<int[]> edges;
         //u ->[[1,10],[2,15],...]
         //      v cost v cost
@@ -71,13 +106,80 @@ public class ex2642 {
             paths.put(node1,dist);
             toReturn=dist[node2];
             return toReturn!=Integer.MAX_VALUE?toReturn:-1;
-        }
-    }
+        }*/
 
-/**
- * Your Graph object will be instantiated and called as such:
- * Graph obj = new Graph(n, edges);
- * obj.addEdge(edge);
- * int param_2 = obj.shortestPath(node1,node2);
- */
+        //dijcstra
+        /*
+        static class Vertex implements Comparable<Vertex>{
+            final int dist;
+            final int vertexInd;
+            public Vertex(int dist, int vertexInd) {
+                this.dist = dist;
+                this.vertexInd = vertexInd;
+            }
+            @Override
+            public int compareTo(Vertex o) {
+                return Integer.compare(dist, o.dist);
+            }
+            @Override
+            public boolean equals(Object o) {
+                if (!(o instanceof Vertex vertex)) return false;
+                //System.out.println("EQUALS: "+vertexInd+", "+vertex.vertexInd);
+                return vertexInd == vertex.vertexInd;
+            }
+            @Override
+            public int hashCode() {
+                return Objects.hash(vertexInd);
+            }
+
+            @Override
+            public String toString() {
+                return "Vertex{" +
+                        "dist=" + dist +
+                        ", vertexInd=" + vertexInd +
+                        '}';
+            }
+        }
+        ArrayList<ArrayList<Vertex>> adjacency;
+
+        public Graph(int n, int[][] edges) {
+            adjacency=new ArrayList<ArrayList<Vertex>>(n);
+            for(int i=0;i<n;++i){
+                adjacency.add(new ArrayList<>());
+            }
+            for(var edge:edges){
+                adjacency.get(edge[0]).add(new Vertex(edge[2], edge[1]));
+            }
+        }
+
+        public void addEdge(int[] edge) {
+            adjacency.get(edge[0]).add(new Vertex(edge[2], edge[1]));
+        }
+
+        public int shortestPath(int node1, int node2) {
+            int toReturn;
+            var distances=new int[adjacency.size()];
+            Arrays.fill(distances,Integer.MAX_VALUE);
+            distances[node1]=0;
+
+            PriorityQueue<Vertex> queue=new PriorityQueue<>(adjacency.size());
+            queue.add(new Vertex(0, 0));
+            while (!queue.isEmpty()){
+                var current=queue.remove();
+                if(current.vertexInd==node2)return current.dist!=Integer.MAX_VALUE?current.dist:-1;
+                var siblings=adjacency.get(current.vertexInd);
+                for(var sibling:siblings){
+                    if(distances[sibling.vertexInd]-sibling.dist > current.dist){
+                        var updatedSibling= new Vertex(distances[sibling.vertexInd] =
+                                current.dist + sibling.dist, sibling.vertexInd);
+                        queue.remove(updatedSibling);
+                        queue.add(updatedSibling);
+                    }
+                }
+            }
+            toReturn=distances[node2];
+            return toReturn!=Integer.MAX_VALUE?toReturn:-1;
+        }
+         */
+    }
 }
