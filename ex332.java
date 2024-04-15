a61383fde3a26ca8e90cbe9259080a1a1d7b4c2d
Vasiliy_Yudin <vasiliy_yudin_education@mail.ru>
2024-04-15 04:34:08 +0300

    normal ex332

--- ex332.java
+++ ex332.java
@@ -1,24 +1,77 @@
 package org.leetcode;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class ex332 {
-    public static void main(String[] args){
-        var sol= new Solution();
-        List<List<String>> tickets=new ArrayList<List<String>>();
-        String[][] ticketsArr=new String[][]{
-                {"JFK","SFO"},{"JFK","ATL"},{"SFO","JFK"},{"ATL","AAA"},
-        {"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"}
-        ,{"AAA","BBB"},{"BBB","ATL"}
+    public static void main(String[] args) {
+        var sol = new Solution();
+        List<List<String>> tickets = new ArrayList<List<String>>();
+        String[][] ticketsArr = new String[][]{
+                {"JFK", "KUL"}, {"JFK", "NRT"}, {"NRT", "JFK"}
         };
-        System.out.print(sol.findItinerary(Arrays.stream(ticketsArr).map(ticket->List.of(ticket[0],ticket[1])).toList()));
+        System.out.print(sol.findItinerary(Arrays.stream(ticketsArr).map(ticket -> List.of(ticket[0], ticket[1])).toList()));
     } //модульный тест
+
     private static class Solution {
         public List<String> findItinerary(List<List<String>> tickets) {
+            //new idea
+            //remembered that what we need to find is an Eulerian cycle
 
+            //the essence of the algorithm: we do the same traversal as
+            //the slow algorithm below, but without unnecessary actions with edges.
+            //We will delete an edge and add the minimal (lexicographically)
+            //neighboring vertex to the stack. Then build the path by adding
+            //vertices from the end when the stack stops filling,
+            //because the vertex will have no more edges.
 
-            //silly, but works
-            HashMap<String, ArrayList<String>> adjacencyMap = new HashMap<>();
+            //Why this should work:
+            //There can be only one "dead-end" branch in a graph,
+            //where the Eulerian path can end.
+            //1) If we go from a vertex to a branch hat has
+            //no end to the Euler path, then we will put all the
+            //necessary vertices on the stack and continue the traversal
+            //in another branch until we reach the end, in which case
+            // the stack will be filled with the desired path in the opposite direction
+            //2) We entered a fork with an end to the path before we entered a
+            // fork without an end to the path. Then we will put into the path
+            // what we have already found, and then we will continue the search
+            // in the remaining return branches and add their bypass to the beginning of the path
+            HashMap<String, PriorityQueue<String>> adjacencyMap = new HashMap<>();
+            String from;
+            String to;
+            for (var ticket : tickets) {
+                from = ticket.getFirst();
+                to = ticket.getLast();
+                adjacencyMap.computeIfAbsent(from, key -> new PriorityQueue<>()).add(to);
+            }
+            //adjacencyMap.forEach((key, queue) -> System.out.println(key + ": " + queue));
+            //System.out.println("=====================");
+            Deque<String> stack = new ArrayDeque<>(tickets.size() + 1);
+            var path = new LinkedList<String>();
+            stack.add("JFK");
+            String current;
+            PriorityQueue<String> siblings;
+            while (!stack.isEmpty()) {
+                siblings = adjacencyMap.get(stack.peek());
+                while (siblings != null && !siblings.isEmpty()) {
+                    stack.push(current = siblings.remove());
+                    siblings = adjacencyMap.get(current);
+                }
+                //System.out.println(stack);
+                path.addFirst(stack.pop());
+                //System.out.println(path);
+                //System.out.println("------------");
+            }
+            return path;
+
+                //silly, but works
+                //bad decision to add to the stack all siblings at once,
+                // but we can add one sibling node to stack and add another
+                // when return to vertex and find this another sibling
+                // in this case there are a lot of unnecessary actions with edges
+            /*HashMap<String, ArrayList<String>> adjacencyMap = new HashMap<>();
             String from;
             String to;
             for (var ticket : tickets) {
@@ -58,7 +111,7 @@ public class ex332 {
                     //System.out.println("---------------");
                 }
             }
-            return path;
+            return path;*/
         }
     }
 }
